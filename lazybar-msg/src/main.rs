e69e2aaa093dbe4512bc3a2c2828e095c315dec0
Jeremy Smart <jeremy3141592@gmail.com>
2024-07-06 01:54:21 -0400

    lazybar-msg: improve help message, add -a flag for easy shutdown

--- lazybar-msg/src/main.rs
+++ lazybar-msg/src/main.rs
@@ -1,3 +1,5 @@
+use std::{ffi::OsString, fs::read_dir, path::PathBuf};
+
 use anyhow::Result;
 use clap::Parser;
 use tokio::{io::AsyncWriteExt, net::UnixStream};
@@ -5,9 +7,13 @@ use tokio::{io::AsyncWriteExt, net::UnixStream};
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// The name of the bar to send the message to
-    bar: String,
-    /// The message to send, in the format `<panel_name>.<message>`
+    /// The name of the bar to which to send the message.
+    #[arg(short, long)]
+    bar: Vec<String>,
+    /// Send the message to all bars, ignoring all instances of `--bar`
+    #[arg(short, long)]
+    all: bool,
+    /// The message to send, in the format `<panel_name>.<message>`.
     message: String,
 }
 
@@ -15,14 +21,27 @@ struct Args {
 async fn main() -> Result<()> {
     let args = Args::parse();
 
-    let path = format!("/tmp/lazybar-ipc/{}", args.bar);
+    let paths = if args.all {
+        read_dir("/tmp/lazybar-ipc/")?
+            .filter_map(|r| r.map(|f| f.path()).ok())
+            .collect::<Vec<_>>()
+    } else {
+        args.bar
+            .iter()
+            .map(|b| {
+                PathBuf::from(OsString::from(format!("/tmp/lazybar-ipc/{b}")))
+            })
+            .collect()
+    };
 
-    let mut stream = UnixStream::connect(path).await?;
+    for path in paths {
+        let mut stream = UnixStream::connect(path).await?;
 
-    stream.writable().await?;
-    stream.try_write(args.message.as_bytes())?;
+        stream.writable().await?;
+        stream.try_write(args.message.as_bytes())?;
 
-    stream.shutdown().await?;
+        stream.shutdown().await?;
+    }
 
     Ok(())
 }
